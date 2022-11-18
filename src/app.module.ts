e518bd4134ff8b2368ae96eaf24eaf03d7c13f82
Keven <80492657+KevenGoncalves@users.noreply.github.com>
2022-11-18 16:21:21 +0200

    nest rest API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { CatsModule } from './cats/cats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { Cat } from './cats/entities/cat.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CatsModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      port: +process.env.DATABASE_PORT!,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [Cat],
+      synchronize: true,
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
---
+++ src/cats/dto/create-cat.dto.ts
@@ -0,0 +1,5 @@
+export class CreateCatDto {
+  firstName: string;
+  lastName: string;
+  age: string;
+}
